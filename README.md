583e48579c35a7eb01aa6b060c1a7eec414e0c8e
Malte Zoudlik <maltezoudlik@gmail.com>
2021-10-25 21:40:52 +0200

    added necessarry classes and some example markup

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Cookie Manager
+
+
+## Markup Examples
+
+### Cookie Manager
+```html
+<cookie-manager>
+</cookie-manager>
+```
+
+### Cookie Placeholder
+```html
+<cookie-placeholder
+  cookie-group="google-maps"
+  cookie-type="iframe"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2511.271907356143!2d6.813878316336119!3d50.992647356028286!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bf30bb1a56da61%3A0xd69b694a1709d92f!2sFLEXX%20Fitness%20%26%20Kurse%20Pulheim!5e0!3m2!1sde!2sde!4v1635189474743!5m2!1sde!2sde"
+  allowfullscreen=""
+  loading="lazy">
+  <cookie-informations></cookie-informations>
+  <cookie-accept-button cookie-group="google-maps">Accept Cookies</cookie-accept-button>
+</cookie-placeholder>
+```
--- src/index.js
+++ src/index.js
@@ -2,13 +2,58 @@ class CookieManager extends HTMLElement {
   constructor() {
     super();
   }
-}
 
-class CookiePlaceholder extends HTMLElement {
+  accept(key) {
+    localStorage.setItem(key, true)
+  }
+  decline(key) {
+    localStorage.setItem(key, false)
+  }
+}
+class CookieAcceptButton extends HTMLElement {
+  constructor() {
+    super();
+
+    this.addEventListener('click', this.handleClick.bind(this))
+  }
+
+  handleClick() {
+    console.dir(this.getAttribute('cookie-group'));
+    let accepted = document.querySelector('cookie-placeholder[cookie-group="google-maps"]');
+
+    console.log(accepted.attributes);
+  }
+}
+class CookieInformations extends HTMLElement {
   constructor() {
     super();
   }
 }
 
+class CookiePlaceholder extends HTMLElement {
+  constructor() {
+    super();
+    this.accepted = false;
+  }
+
+  transformToIframe() {
+
+  }
+
+  transformToScript() {
+
+  }
+
+  transformToPlaceholder() {
+
+  }
+
+  accept() {
+    this.accepted = true;
+  }
+}
+
 customElements.define('cookie-manager', CookieManager);
+customElements.define('cookie-accept-button', CookieAcceptButton);
+customElements.define('cookie-informations', CookieInformations);
 customElements.define('cookie-placeholder', CookiePlaceholder);
